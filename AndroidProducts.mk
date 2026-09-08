fee26d5bcb39dfb7ff3922a584cee19935230690
rk3288 <m18749410674@163.com>
2026-09-08 14:30:39 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,6 +11,6 @@ COMMON_LUNCH_CHOICES := \
 	twrp_T1030M-user \
 	twrp_T1030M-userdebug \
 	twrp_T1030M-eng \
-	twrp_T1030M-bp2a-user \
-	twrp_T1030M-bp2a-userdebug \
-    twrp_T1030M-bp2a-eng
+	twrp_T1030M-ap2a-user \
+	twrp_T1030M-ap2a-userdebug \
+    twrp_T1030M-ap2a-eng
